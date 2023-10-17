5aab48997df9249e974fc078a264b0619e79e31a
SSANJUA <ssanjua@alumno.upv.es@linux-vdi02.upvnet.upv.es>
2023-10-17 12:33:12 +0200

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,5 +1,6 @@
 #include <ostream>
 #include "List.h"
+#include <algorithm>
 
 template <typename T>
 
@@ -23,107 +24,149 @@ class ListArray : public List<T> {
   friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
 
 
-  void insert(int pos, T e) override;
-  void append(T e) override;
-  void prepend(T e) override;
-  T remove(int pos) override;
-  T get(int pos) override;
-  int search(T e) override;
-  int size() override;
-  bool empty() override;
+  void insert(int pos, T e) override{
+
+    if (pos >= 0 && pos < n) {
+      arr[pos] = e;
+    }
+  }
+  
+  void append(T e) override{
+    if (n == max) {
+      resize(1 + max);
+    }
+    arr[n] = e;
+    n++;
+  }
+  
+  void prepend(T e) override{
+    if (n == max) {
+      resize(2 * max);
+    }
+    for (int i = n; i > 0; i--) {
+      arr[i] = arr[i - 1];
+    }
+    arr[0] = e;
+    n++;
+  }
+  
+  T remove(int pos) override{
+
+    	 T aux;
+	int i;
+  	if (pos < 0 || pos >= n) {
+      throw std::out_of_range("Índice fuera de los límites de la lista");
+    	}
+   	 T aux = arr[pos];
+	arr[pos]=nullptr;
+    	for (i = pos; i < n - 1; i++) {
+    	  arr[i] = arr[i + 1];
+   	 }
+	arr[max-1]=nullptr;
+   	 n--;
+   	 return aux;
+  }
+  
+  T get(int pos) override{
+    T aux;
+
+if (pos < 0 || pos >= n) {
+      throw std::out_of_range("Índice fuera de los límites de la lista");
+    }
+     aux = arr[pos];
+ 
+     return aux;
+  }
+  
+  int search(T e) override{
+    int i= 0;
+    while(arr[i]!= e){
+      i++;
+      if(i >= max){
+	return -1;
+	  }
+    }
+    return i;
+  }
+  
+  int size() override{
+    int i,j = 0;
+    
+    for(i = 0; i<max;i++){
+      if(arr[i]!='/0'){
+	j++;   
+      }
+    }
+
+    return j;
+  }
+  
+  bool empty() override{
+
+     return n == 0;   
+  }
   
   
 };
 
-ListArray::ListArray(){
-
+template <typename T>
+ListArray<T>::ListArray(){
+ arr = new T[MINSIZE];
+  max = MINSIZE;
+n = 0;
 }
 
-ListArray::~ListArray(){}
-
 template <typename T>
-void List<T>::insert(int pos, T e) override{
-  try{    
-    this-> arr[pos]=e;
-  }
-  catch(const std::out_of_range& oor) {
-    std::cerr << "Out of Range error: " << oor.what() << '\n';
-  }
-
+ListArray<T>::~ListArray(){
+  delete[] arr;
 }
-  
-  template <typename T>       
-  void List<T>::append(T e) override{
-    this->arr[max-1] = e;
-  }
-
-
- template <typename T>       
-  void List<T>::prepend(T e) override{
-    this->arr[0] = e;
-  }
-
-
- template <typename T>
- T List<T>::remove(int pos) override{
-   T aux;
-   try{
-     aux = arr[pos];
-    this-> arr[pos]= '/0';
-  }
-  catch(const std::out_of_range& oor) {
-    std::cerr << "Out of Range error: " << oor.what() << '\n';
-  }
-  return aux;
- }
 
 
 
 template <typename T>
- T List<T>::get(int pos) override{
-   T aux;
-   try{
-     aux = arr[pos];
-  }
-  catch(const std::out_of_range& oor) {
-    std::cerr << "Out of Range error: " << oor.what() << '\n';
-  }
-  return aux;
- }
+T ListArray<T>::operator[](int pos){
 
-template <typename T>
-int List<T>::search(T e) override{
-  int i= 0;
-  while(arr[i]!= e){
-     i++;
-    if(i >= max){
-      return -1
+  try{
+  if(pos < 0 || pos >= max){
+    throw std::out_of_range("Índice fuera de los límites del array");
+    
+  }
+  else{
+    return arr[pos];
+  }
+  }
+  catch (const std::out_of_range& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
-  }
-  return i;
+
 }
 
 template <typename T>
-int List<T>::size() override{
-  int i,j = 0;
-  for(i = 0; i<max;i++){
-    if(arr[i]!='/0'){
-      j++;   
-    }
-  }
-
-  return j;
-}
-
-template <typename T>
-bool List<T>::empty() override{
+std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
   int i;
-  for(i = 0; i<max; i++){
-   if(arr[i]!='/0'){
-     return false;   
+  out << "[";
+    for (int i = 0; i < max; ++i) {
+        out << arr[i];
+        if (i < max-1) {
+            out << ", ";
+        }
     }
-  }
-
-  return true;
+    out << "]";
+    return out;
+  
 }
+
+ void ListArray<T>::resize(int new_size){
+  int* m = new int[new_size];
+
+  for(int i = 0; i< n; i++){
+	m[i] = arr[i];
+}
+
+  delete[] arr;
+
+  arr = m;
+
+  max = new_size;
+  }
 
