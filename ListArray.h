17d6e52e1fd895d478fdabdde708842bac217e8b
SaulSJ5 <ssanjua@upv.edu.es>
2023-11-14 10:39:26 +0100

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,6 +1,7 @@
 #include <ostream>
 #include "List.h"
-#include <algorithm>
+#include <vector>
+#include <iostream>
 
 template <typename T>
 
@@ -12,26 +13,26 @@ class ListArray : public List<T> {
   int n;
   static const int MINSIZE = 2;
 
-  void resize(int new_size);
 
  public:
-  ListArray();
-  
-  ~ListArray();
-  
-  T operator[](int pos);
-  
-  friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
 
 
   void insert(int pos, T e) override{
-
-    if (pos >= 0 && pos < n) {
-      arr[pos] = e;
+    if (pos < 0 || pos > max-1) {
+      throw std::out_of_range("Índice fuera de los límites de la lista");
     }
+    if (n == max) {
+      resize(1 + max);
+    }
+    for (int i = n; i > pos; i--) {
+      arr[i] = arr[i - 1];
+    }
+    arr[pos] = e;
+    n++;
   }
   
   void append(T e) override{
+    
     if (n == max) {
       resize(1 + max);
     }
@@ -40,8 +41,9 @@ class ListArray : public List<T> {
   }
   
   void prepend(T e) override{
+    
     if (n == max) {
-      resize(2 * max);
+      resize(1 + max);
     }
     for (int i = n; i > 0; i--) {
       arr[i] = arr[i - 1];
@@ -51,31 +53,28 @@ class ListArray : public List<T> {
   }
   
   T remove(int pos) override{
-
-    	 T aux;
-	int i;
-  	if (pos < 0 || pos >= n) {
+   if (pos < 0 || pos > n-1) {
       throw std::out_of_range("Índice fuera de los límites de la lista");
-    	}
-   	 T aux = arr[pos];
-	arr[pos]=nullptr;
-    	for (i = pos; i < n - 1; i++) {
-    	  arr[i] = arr[i + 1];
-   	 }
-	arr[max-1]=nullptr;
-   	 n--;
-   	 return aux;
+    }
+   T m = arr[pos];
+   
+   for(int i = pos; i < max; i++){
+     arr[i] = arr[i + 1];
+   }
+   max--;
+   n--;
+   resize(max);
+   return m;
   }
   
   T get(int pos) override{
-    T aux;
+    
 
-if (pos < 0 || pos >= n) {
+    if (pos < 0 || pos > n-1) {
       throw std::out_of_range("Índice fuera de los límites de la lista");
     }
-     aux = arr[pos];
  
-     return aux;
+     return arr[pos];
   }
   
   int search(T e) override{
@@ -90,15 +89,9 @@ if (pos < 0 || pos >= n) {
   }
   
   int size() override{
-    int i,j = 0;
     
-    for(i = 0; i<max;i++){
-      if(arr[i]!='/0'){
-	j++;   
-      }
-    }
 
-    return j;
+    return n;
   }
   
   bool empty() override{
@@ -106,27 +99,19 @@ if (pos < 0 || pos >= n) {
      return n == 0;   
   }
   
-  
-};
-
-template <typename T>
-ListArray<T>::ListArray(){
+  ListArray(){
  arr = new T[MINSIZE];
   max = MINSIZE;
 n = 0;
 }
 
-template <typename T>
-ListArray<T>::~ListArray(){
+  ~ListArray(){
   delete[] arr;
 }
 
+  T operator[](int pos){
 
-
-template <typename T>
-T ListArray<T>::operator[](int pos){
-
-  try{
+  
   if(pos < 0 || pos >= max){
     throw std::out_of_range("Índice fuera de los límites del array");
     
@@ -134,39 +119,52 @@ T ListArray<T>::operator[](int pos){
   else{
     return arr[pos];
   }
-  }
-  catch (const std::out_of_range& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+ 
 
 }
-
-template <typename T>
-std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+  
+  friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
   int i;
   out << "[";
-    for (int i = 0; i < max; ++i) {
-        out << arr[i];
-        if (i < max-1) {
+    for (int i = 0; i < list.max; ++i) {
+        out << list.arr[i];
+        if (i < list.max-1) {
             out << ", ";
         }
     }
     out << "]";
     return out;
   
-}
+    }
 
- void ListArray<T>::resize(int new_size){
-  int* m = new int[new_size];
+  
+ void resize(int new_size){
+  T* m = new T[new_size];
 
-  for(int i = 0; i< n; i++){
-	m[i] = arr[i];
-}
+  for(int i = 0; i < n; i++){
+    m[i] = arr[i];
+  }
 
   delete[] arr;
-
+  max = new_size;
   arr = m;
 
-  max = new_size;
+  
   }
+ 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
